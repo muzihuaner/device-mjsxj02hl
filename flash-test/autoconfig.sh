9777442390a4469b1fef3b0dbf9df400ef9c3505
muzihuaner <muzihuaner@hotmail.com>
2026-03-21 21:35:29 +0800

    更新

---
+++ flash-test/autoconfig.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# First boot
+#
+
+# Set environments (mmz memory = 64 - osmem)
+fw_setenv bootargs 'mem=${osmem} console=ttyAMA0,115200 panic=20 rootfstype=squashfs root=/dev/mtdblock3 init=/init mtdparts=${mtdparts} mmz_allocator=hisi'
+fw_setenv osmem 35M
+
+# Enable white LED
+$(dirname $0)/autoconfig/usr/sbin/led_control.sh -o 1 -b 1
+
+# Wipe overlay partition and reboot
+flash_eraseall -j /dev/mtd4
+reboot
